5cdd8e5c6c1236f3175bb378635b4daec2fad885
ASFedotov <asfedotov1988as@gmail.com>
2022-12-04 15:00:01 +0500

    add Example005_Condition_If_else

---
+++ Examples/Example005_Condition_If_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "pavel")
+{
+    Console.WriteLine("Ура, это же Pavel!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
