b08e6987191056c12b5263024d065d95e3db7272
ASFedotov <asfedotov1988as@gmail.com>
2022-12-08 22:56:50 +0500

    add Changes Ex_009

--- Examples/Example009_Intro_Array/Program.cs
+++ Examples/Example009_Intro_Array/Program.cs
@@ -1,72 +1,18 @@
-﻿// int Max(int arg1, int arg2, int arg3)
-// {
-//     int result = arg1;
-//     if(arg2 > result) result = arg2;
-//     if(arg3 > result) result = arg3;
-//     return result;
-// }
-
-// int[] array = {11, 21, 31, 41, 15, 61, 17, 18, 19};
-// array[0] = 12;
-
-// Console.WriteLine(array[0]);
-
-
-
-
-
-void FillArray(int[] collection) 
- {
-    int length = collection.Length;
-    int index = 0;
-    while (index < length)
-    {
-
-        collection[index] = new Random().Next(1, 10);
-        index++;
-    }
- }
-
-void PrintArray(int[] col)
+﻿int Max(int arg1, int arg2, int arg3)
 {
-    int count = col.Length;
-    int position = 0;
-    
-    while (position < count)
-    {
-        Console.WriteLine(col[position]);
-        position++;
-    }
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
 }
 
-int IndexOf(int[] collection, int find)
-{
-    int count = collection.Length;
-    int index = 0;
-    int position = -1;
+int[] array = {11, 21, 31, 41, 15, 61, 17, 18, 19};
+array[0] = 12;
 
-    while(index < count)
-    {
-        if(collection[index] == find)
-        {
-            position = index; 
-            break; 
-        }
-        index++;
-    }
-    return position;
-}
+Console.WriteLine(array[0]);
 
-int[] array = new int[10];
 
-FillArray(array);
 
-array[4] = 4;
-array[7] = 4;
 
-PrintArray(array);
-Console.WriteLine();
 
-int pos = IndexOf(array, 4);
-Console.WriteLine(pos);
 
